20a64bc57c0c1648728127ea90fe79776c9f0ec0
gaprass7 <gnngzr@gmail.com>
2025-10-05 14:39:15 +0700

    add migrasi

---
+++ app/Models/Slider.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+
+class Slider extends Model
+{
+    protected $guarded = [];
+}
